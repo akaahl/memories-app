6d8a650a5aa9558c8e431e895d05593851047672
a.k.a.a.h.l <a.k.afiq@live.com>
2021-07-24 21:01:38 +0800

    Added client components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import memories from "./assets/memories.png";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <p>This is a test project</p>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} alt="memories" height="60" />
+      </AppBar>
+
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          ></Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 };
 
