dd8c56a9e3cd3c3b6baeeaf7ef5e8815b3f6d6b8
a.k.a.a.h.l <a.k.afiq@live.com>
2021-07-24 19:23:09 +0800

    Another commit

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import express from "express";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import postRoutes from "./routes/posts.js";
 
-dontenv.config();
+dotenv.config();
 
 const app = express();
+
+// middleware routes
+app.use("/posts", postRoutes);
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -14,4 +17,14 @@ app.use(cors());
 
 // connect to db
 const CONNECTION_URL = process.env.CONNECTION_URL;
-const PORT = process.env.PORT || PORT;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => app.listen(PORT, () => console.log(`Server up on port: ${PORT}`)))
+  .catch((err) => console.log(err.message));
+
+mongoose.set("useFindAndModify", false);
