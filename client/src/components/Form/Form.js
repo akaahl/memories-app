6b090bc3b5610124a0f462812dd1e18b28fc8755
a.k.a.a.h.l <a.k.afiq@live.com>
2021-07-25 18:59:51 +0800

    Added material files into the Form component

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,11 +1,43 @@
+import { useState } from "react";
 import useStyles from "./styles";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
 
 const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+
   const classes = useStyles();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChange = () => {};
+
   return (
-    <div>
-      <h1>Form</h1>
-    </div>
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a memory</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={handleChange}
+        />
+      </form>
+    </Paper>
   );
 };
 
