38cf3c82eb72ed114478a9c2a8cb48753033f05e
a.k.a.a.h.l <a.k.afiq@live.com>
2021-07-26 18:47:46 +0800

    Added updatedPost route and controller

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 import memories from "./assets/memories.png";
 import Posts from "./components/Posts/Posts.js";
@@ -10,6 +10,7 @@ import { getPosts } from "./actions/posts";
 const App = () => {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const [currentId, setCurrentId] = useState(null);
 
   useEffect(() => {
     dispatch(getPosts());
@@ -38,11 +39,11 @@ const App = () => {
             spacing={3}
           >
             <Grid item xs={12} sm={7}>
-              <Posts />
+              <Posts setCurrentId={setCurrentId} />
             </Grid>
 
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
