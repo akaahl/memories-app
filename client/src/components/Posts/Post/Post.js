04f28cc2c5879e8f6e03508fe622e10ddd5aaabe
a.k.a.a.h.l <a.k.afiq@live.com>
2021-07-27 16:57:22 +0800

    Fixed a bug in Post component

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -42,7 +42,7 @@ const Post = ({ post, setCurrentId }) => {
 
       <div className={classes.details}>
         <Typography variant="body2" color="textSecondary">
-          {post.tags.map((tag) => `#${tag}`)}
+          {post.tags.map((tag) => `#${tag} `)}
         </Typography>
       </div>
 
