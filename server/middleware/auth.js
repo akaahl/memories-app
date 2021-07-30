1f345cad312ba6ed56489a186a07f7b0ba0ee7ae
a.k.a.a.h.l <a.k.afiq@live.com>
2021-07-30 21:58:15 +0800

    Created auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const isCustomAuth = token.length < 500;
+
+    let decodedData;
+
+    if (token && isCustomAuth) {
+      decodedData = jwt.verify(token, process.env.SECRET_STRING);
+
+      req.userId = decodedData?.id;
+    } else {
+      decodedData = jwt.decode(token);
+
+      // sub is google's proprietary algorithm to differentiate different users
+      req.userId = decodedData?.sub;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
