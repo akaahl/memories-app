1e59778377ca4643b25b5b91a60abd7d953ff34c
a.k.a.a.h.l <a.k.afiq@live.com>
2021-08-03 19:21:51 +0800

    Changed background color

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,5 +1,6 @@
 import {
   FETCH_ALL,
+  FETCH_BY_SEARCH,
   CREATE,
   UPDATE,
   DELETE,
@@ -23,7 +24,7 @@ export const getPostsBySearch = (searchQuery) => async (dispatch) => {
       data: { data },
     } = await api.fetchPostsBySearch(searchQuery);
 
-    console.log(data);
+    dispatch({ type: FETCH_BY_SEARCH, payload: data });
   } catch (err) {
     console.log(err);
   }
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,14 +1,18 @@
 import {
   FETCH_ALL,
+  FETCH_BY_SEARCH,
   CREATE,
   UPDATE,
   DELETE,
   LIKE,
 } from "../constants/actionTypes";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
+      return action.payload;
+    case FETCH_BY_SEARCH:
       return action.payload;
     case LIKE:
       return posts.map((post) =>
